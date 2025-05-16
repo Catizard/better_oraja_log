855b457d3db469ccae7fa5df27c02adb548998a1
Catizard <1185032459@qq.com>
2025-05-16 10:23:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Better Oraja Log
+
+This repo holds a small script for hacking the `scoredatalog.db` from `beatoraja` and its variants.The original problem is `beatoraja` doesn't record down every logs correctly.
+
+## How to use
+
+> BACKUP YOUR OWN FILE BEFORE DOING ANYTHING!
+
+This is a tui application, put it at the same directory with your `scoredatalog.db` and open it at your terminal and it would pop up a form for you selecting the `scoredatalog.db` file path which should be located at same directory. Then pressing enter, a new file called `better_scoredatalog.db` would be generated at the same directory. Replace your old `scoredatalog.db` file with it.
+
+
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/huh"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+type ScoreDataLog struct {
+	Sha256     string
+	Mode       string
+	Clear      int32
+	Epg        int32
+	Lpg        int32
+	Egr        int32
+	Lgr        int32
+	Egd        int32
+	Lgd        int32
+	Ebd        int32
+	Lbd        int32
+	Epr        int32
+	Lpr        int32
+	Ems        int32
+	Lms        int32
+	Notes      int32
+	Combo      int32
+	Minbp      int32
+	PlayCount  int32
+	ClearCount int32
+	Option     int32
+	Seed       int64
+	Random     int32
+	TimeStamp  int64
+	State      int32
+}
+
+func (ScoreDataLog) TableName() string {
+	return "scoredatalog"
+}
+
+const outputPath = "better_scoredatalog.db"
+
+func main() {
+	if _, err := os.Stat(outputPath); err != nil {
+		if !os.IsNotExist(err) {
+			panic(err)
+		}
+	} else {
+		panic(fmt.Errorf("%s existed, please remove it first", outputPath))
+	}
+	var prevFilePath string
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewFilePicker().Title("scoredatalog.db:").Value(&prevFilePath),
+		),
+	)
+
+	if err := form.Run(); err != nil {
+		panic(err)
+	}
+
+	// Load everything into memory
+	dsn := prevFilePath + "?open_mode=1"
+	db, err := gorm.Open(sqlite.Open(dsn))
+	if err != nil {
+		panic(err)
+	}
+	var rawLogs []*ScoreDataLog
+	if err := db.Find(&rawLogs).Error; err != nil {
+		panic(err)
+	}
+
+	// Output to another file
+	outputDSN := fmt.Sprintf("%s?open_mode=1", outputPath)
+	outputDB, err := gorm.Open(sqlite.Open(outputDSN))
+	if err := outputDB.Table("scoredatalog").AutoMigrate(&ScoreDataLog{}); err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := outputDB.Model(&ScoreDataLog{}).CreateInBatches(rawLogs, 50).Error; err != nil {
+		panic(err)
+	}
+}
